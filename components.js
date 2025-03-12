2a1363e572b19dc07e0ca7e0f5796c2cd6366048
jasmin <zimmer.jasmin04@gmail.com>
2025-03-12 13:14:28 +0100

    Header als Komponenten anlegen

--- components.js
+++ components.js
@@ -1,3 +1,22 @@
 $(document).ready(function () {
   $("#footer").load("./components/footer.html");
 });
+
+$(document).ready(function () {
+  $("#header").load("./components/header.html", function () {
+    let aktuelleSeite = window.location.pathname.split("/").pop();
+    console.log(aktuelleSeite);
+
+    switch (aktuelleSeite) {
+      case "werdegang.html":
+        $(".header-heading").text("Mein Werdegang");
+        break;
+      case "buchfavoriten.html":
+        $(".header-heading").text("Meine Buchfavoriten");
+        break;
+      case "ueber-mich.html":
+        $(".header-heading").text("Über mich");
+        break;
+    }
+  });
+});
